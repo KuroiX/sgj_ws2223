bd2a65a8f7732ba0d94b8ffd22eedc3eca6ff9e9
KuroiX <geheeb@in.tum.de>
2022-12-10 02:09:36 +0100

    InputManager

---
+++ OfficeTrouble/Assets/Scripts/Key/InputManager.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
+
+public class InputManager
+{
+    public static InputManager Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    [SerializeField]
+    private GameObject visual;
+
+    private Dictionary<string, bool> uiKeyPressedDict;
+
+
+    public void UIKeyPressed(string key)
+    {
+        if (uiKeyPressedDict.ContainsKey(key))
+        {
+            uiKeyPressedDict[key] = true;
+        }
+        else
+        {
+            uiKeyPressedDict.Add(key, true);
+        }
+    }
+
+    public void UIKeyReleased(string key)
+    {
+        if (uiKeyPressedDict.ContainsKey(key))
+        {
+            uiKeyPressedDict[key] = false;
+        }
+        else
+        {
+            // should never happen!
+            uiKeyPressedDict.Add(key, true);
+        }
+    }
+
+    public bool KeyIsPressed(string key)
+    {
+        bool uiKeyPressed = uiKeyPressedDict.ContainsKey(key) && uiKeyPressedDict[key];
+        return uiKeyPressed || ((KeyControl)Keyboard.current[key]).wasReleasedThisFrame;
+    }
+
+}
