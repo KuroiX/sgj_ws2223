bdcc603d74ccf08402cfe92879d06a9b30aec6dc
DominikHuber <dominikhuber47@gmail.com>
2022-12-11 10:47:38 +0100

    menu design

--- OfficeTrouble/Assets/Scripts/GameController.cs
+++ OfficeTrouble/Assets/Scripts/GameController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
@@ -71,7 +72,8 @@ public class GameController : MonoBehaviour
 		{
 			Debug.Log("YOU LOST!");
             AudioManagerScript.Instance.GameIsLost();
-		}
+            SceneManager.LoadScene("GameOver");
+        }
 	}
 
     private void FixedUpdate()
